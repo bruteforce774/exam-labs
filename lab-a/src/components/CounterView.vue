dfcb83f3afc0777b066d999c55875c6a12111f29
Daniel Andreas Wang <danielaw@getacademy.no>
2026-01-03 23:45:31 +0100

    wip

---
+++ lab-a/src/components/CounterView.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+    const props = defineProps<{
+        count: number
+    }>()
+
+    const emits = defineEmits<{
+        increment: [],
+        decrement: []
+    }>()
+</script>
+
+<template>
+  <div>
+    count: {{ count }}<br />
+    <button @click="emits('increment')">Increment</button>
+    <button @click="emits('decrement')">Decrement</button>
+  </div>
+</template>
+
+<style>
+</style>
